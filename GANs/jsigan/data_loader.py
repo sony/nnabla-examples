2458a382eb52b907c8f3c2402bcabff2b2e8287e
woody.li <woody.li@sony.com>
2021-06-18 15:35:28 +0800

    update copyright declaration of each source file

--- GANs/jsigan/data_loader.py
+++ GANs/jsigan/data_loader.py
@@ -1,8 +1,9 @@
-# Copyright (c) 2017 Sony Corporation. All Rights Reserved.
+# Copyright 2021 Sony Corporation.
+# Copyright 2021 Sony Group Corporation.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
-#  may obtain a copy of the License at
+# You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
