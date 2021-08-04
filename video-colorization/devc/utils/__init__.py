27ff677caa76244941a1cb773a3fa98e8a1c07c3
TE-IbinasChaliyadan <ibinas.chaliyadan@sony.com>
2021-08-04 14:30:15 +0530

    remove folder devc

--- video-colorization/devc/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-import os
-import sys
-common_utils_path = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), '..', '..', '..', 'utils'))
-sys.path.append(common_utils_path)
-from neu.yaml_wrapper import read_yaml
