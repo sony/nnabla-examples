1696e8c6021ee6917b8c0c386dbdf23026eb019a
Takuya.Yashima <takuya.yashima@sony.com>
2021-09-22 10:19:36 +0900

    fix download link

--- GANs/reenactgan/download_pretrained_weights.sh
+++ GANs/reenactgan/download_pretrained_weights.sh
@@ -19,7 +19,7 @@ wget -P pretrained_weights/encoder https://nnabla.org/pretrained-models/nnabla-e
 wget -P pretrained_weights/encoder https://nnabla.org/pretrained-models/nnabla-examples/GANs/reenactgan/encoder/training_info.yaml
 
 mkdir -p pretrained_weights/transformer/Kathleen2Donald_Trump
-wget -P pretrained_weights/transformer/Kathleen2Donald_Trump https://nnabla.org/pretrained-models/nnabla-examples/GANs/reenactgan/transformer/pretrained_weights.h5
+wget -P pretrained_weights/transformer/Kathleen2Donald_Trump https://nnabla.org/pretrained-models/nnabla-examples/GANs/reenactgan/transformer/weights.h5
 wget -P pretrained_weights/transformer/Kathleen2Donald_Trump https://nnabla.org/pretrained-models/nnabla-examples/GANs/reenactgan/transformer/training_info.yaml
 
 mkdir -p pretrained_weights/decoder/Donald_Trump
