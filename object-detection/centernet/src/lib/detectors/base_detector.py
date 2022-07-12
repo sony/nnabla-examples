ff80deb835c460d365a80bf358860d07c2234f32
Hsingying Ho <Hsingying.Ho@sony.com>
2022-07-12 21:14:51 +0900

    Remove return_time argument from BaseDetector::process() since the value of return_time is hard-coded and always set to True. Add docstring.

--- object-detection/centernet/src/lib/detectors/base_detector.py
+++ object-detection/centernet/src/lib/detectors/base_detector.py
@@ -88,7 +88,7 @@ class BaseDetector(object):
         }
         return images, meta
 
-    def process(self, images, return_time=False):
+    def process(self, images):
         raise NotImplementedError
 
     def post_process(self, dets, meta, scale=1):
@@ -137,7 +137,7 @@ class BaseDetector(object):
             pre_process_time = time.time()
             pre_time += pre_process_time - scale_start_time
 
-            output, dets, forward_time = self.process(images, return_time=True)
+            output, dets, forward_time = self.process(images)
 
             net_time += forward_time - pre_process_time
             decode_time = time.time()
