a4019d646f220eec7bba7d4325a2aee0538f25be
Hsingying Ho <Hsingying.Ho@sony.com>
2022-07-15 12:40:48 +0900

    Fix flake8 errors.

--- object-detection/centernet/src/save_graph.py
+++ object-detection/centernet/src/save_graph.py
@@ -13,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
