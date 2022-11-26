7e4728df9881d0b0b0911501ed8c3cfae34f9e84
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-26 16:43:36 +0000

    Adding tarfile member sanitization to extractall()

--- image-generation/sagan/create_val_dir.py
+++ image-generation/sagan/create_val_dir.py
@@ -32,7 +32,26 @@ dst_dir = args.outdir
 
 with tarfile.open(source_tar_file) as tf:
     v_tmp_dir = dst_dir + '/' + 'tmpdir'
-    tf.extractall(v_tmp_dir)
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(tf, v_tmp_dir)
 with open(os.path.join(os.path.dirname(__file__), "category_list.txt")) as f:
     categories = f.readlines()
 with open(os.path.join(os.path.dirname(__file__), "val_data_category_list.txt")) as f:
